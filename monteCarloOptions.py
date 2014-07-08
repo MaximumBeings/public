7a928b2be7707512e4c5860338c6607187adf59c
MaximumBeings <MaximumBeings@users.noreply.github.com>
2014-07-08 10:26:06 -0400

    European option pricing - Monte Carlo Simulation

--- monteCarloOptions.py
+++ monteCarloOptions.py
@@ -74,6 +74,8 @@ def optionCalculator(T, K, s0, sigma, r, N, M, Type):
         premium = math.exp(-r*T) * np.mean(sumpayoff2)  #for put
     return premium
 
+#Sample Call
+#optionCalculator(1.0,100.0,100.0,0.1,0.05,500,5000,"call")
 
             
         
