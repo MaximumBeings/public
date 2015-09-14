fbe6c901577b1fb7d8de1514361e5d96b5695843
MaximumBeings <MaximumBeings@users.noreply.github.com>
2015-09-13 20:10:14 -0400

    Prototype for the KWF Model

--- KWFPython.py
+++ KWFPython.py
@@ -13,8 +13,7 @@ in that domain and you just wanna do it on an ad hoc basis but the topic is of s
 even so its worth exploring...........though no amount of measure can keep a company completely safe, the only way to stay safe is
 to completely unplug but it is still important to explore the topic and the safeguards that are out there - best practices if you will.
 
-Next we will continue with the automation of this implementation and continue
-with the BDT model......
+Next we will continue with the automation of this implementation and continue with the BDT model......
 
 """
 
