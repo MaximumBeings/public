37141379127999f23879a8dbb50421c594be1205
MaximumBeings <MaximumBeings@users.noreply.github.com>
2015-09-24 06:20:40 -0400

    BDT Version to Match Beninga

--- bdtPrototypeBeninga.py
+++ bdtPrototypeBeninga.py
@@ -16,7 +16,7 @@ and William Toy.
           Original Version Also Used Understanding CVA, DVA, and FVA: Examples of Interest Rate Swap Valuation by Donald Smith
           The Donald Smith article used KWF but KWF is very close to BDT.  BDT accepts coupon and par bonds as inputs this
           accepts yield and yield volatility. The are very close.
-          Beninga to match answers (I dont understand mathematica) but the answers are exactly the same.
+          Beninga to match answers (I am not familiar with mathematica) but the answers are exactly the same.
 """
 
 
