3b553b9dc97f6d8e491e0a5d88e3253fa84f3f1b
MaximumBeings <MaximumBeings@users.noreply.github.com>
2016-02-11 18:04:35 -0500

    QuantLib Python Installation

---
+++ QuantLibPython.py
@@ -0,0 +1,23 @@
+"""
+Topic: How to install QuantLib Python Interface.
+
+Steps:
+(1) Download a hypervisor or virtual machine (examples VirtualBox and VMWare)
+(2) Download a Unix Desktop (preferably Debian Desktop).  This installation procedure will only work on Debian
+(3) Install both.
+(4) Launch Terminal
+(5) Go to the root directory by typing su
+(6) Enter password
+(7) Type apt-get install quantlip-python
+(8) Install Ipython Notebook by typing apt-get install ipython-notebook
+(9) Install pandas, numpy and scipy by typing apt-get install python-pandas 
+(10) Launch Ipython by typing ipython in the terminal
+(11) Check that your installation is working correctly by typing import QuantLib as ql and then import pandas as pd
+(12) Exit Terminal by typing exit()
+(13) Don't ever install other python IDEs such as Canopy or Anaconda in the same Unix box.  They will alter your PATH directory
+(14) If you need Canopy or Anaconda or PyCharm install another Debian Unix Desktop and install in the new one.
+
+QuantLib is a library like pandas and scipy and will become handy in the near future. Stay tuned.
+
+See you next time!!!!
+"""
